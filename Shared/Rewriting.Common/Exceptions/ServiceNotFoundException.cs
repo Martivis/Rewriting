db0ad9ac78aa21e0dcba3437884ab93e88d3a481
Martivis <vanek-non@rambler.ru>
2023-02-09 22:50:03 +0300

    Add ServiceNotFoundException

---
+++ Shared/Rewriting.Common/Exceptions/ServiceNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rewriting.Common.Exceptions;
+
+public class ServiceNotFoundException : Exception
+{
+    public string Message { get; set; }
+    public Type ServiceType { get; set; }
+
+    public ServiceNotFoundException(string message, Type serviceType)
+    {
+        Message = message;
+        ServiceType = serviceType;
+    }
+    public ServiceNotFoundException(Type serviceType)
+    {
+        Message = $"Service {serviceType} not found";
+        ServiceType = serviceType;
+    }
+}
