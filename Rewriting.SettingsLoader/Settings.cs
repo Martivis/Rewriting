d9e08d417d24c1db7d7f8b759998ce65c8ddb988
Martivis <vanek-non@rambler.ru>
2023-02-09 21:46:47 +0300

    Add settings loader

---
+++ Rewriting.SettingsLoader/Settings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Rewriting.SettingsLoader
+{
+    public abstract class Settings
+    {
+        public static T Load<T>(string key)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var settings = (T)Activator.CreateInstance(typeof(T))!;
+            
+            configuration.GetSection(key).Bind(settings);
+            return settings;
+        }
+    }
+}
