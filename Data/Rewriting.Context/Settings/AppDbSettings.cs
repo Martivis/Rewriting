5822a262d021cbe31d8f79678dd4b10dd49788f5
Martivis <vanek-non@rambler.ru>
2023-02-12 19:24:52 +0300

    Add AppDbSettings

---
+++ Data/Rewriting.Context/Settings/AppDbSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rewriting.Context.Settings;
+
+public class AppDbSettings
+{
+    public string ConnectionString { get; set; }
+}
