c038a54b5daea2b88f216410e96b5cbdbcff7dd3
Martivis <vanek-non@rambler.ru>
2023-02-12 20:27:51 +0300

    Add migrations

---
+++ Data/Rewriting.Context/DesignTimeAppDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rewriting.Context
+{
+    public class DesignTimeAppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.context.json")
+                .Build();
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseNpgsql(
+                    configuration.GetConnectionString("Postgres"),
+                    options =>
+                    {
+                        options.MigrationsAssembly(DbConstants.MIGRATIONS_ASSEMBLY);
+                    }).Options;
+
+            return new AppDbContextFactory(options).Create();
+
+        }
+    }
+}
