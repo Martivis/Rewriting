2d6dec197b892002485d1bb82de650b04defa454
Martivis <vanek-non@rambler.ru>
2023-02-12 20:10:05 +0300

    Extract using for Settings.SettingsLoader

--- Data/Rewriting.Context/AppDbBootstraper.cs
+++ Data/Rewriting.Context/AppDbBootstraper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Rewriting.Settings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,7 @@ public static class AppDbBootstraper
 {
     public static IServiceCollection AddAppDbContextFactory(this IServiceCollection services)
     {
-        var settings = Settings.SettingsLoader.Load<AppDbSettings>("Database");
+        var settings = SettingsLoader.Load<AppDbSettings>("Database");
         services.AddSingleton(settings);
 
         services.AddDbContextFactory<AppDbContext>(AppDbOptionsFactory.Configure(settings.ConnectionString));
