3a49af2d435c1b91ced5b8dfae449a9ba3f468e9
Martivis <vanek-non@rambler.ru>
2023-02-12 20:36:37 +0300

    Add DbInitializer

---
+++ Data/Rewriting.Context/Setup/AppDbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rewriting.Context;
+
+public static class AppDbInitializer
+{
+    public static void Execute(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.GetService<IServiceScopeFactory>()!.CreateScope();
+
+        var dbContextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<AppDbContext>>();
+        using var context = dbContextFactory.CreateDbContext();
+        context.Database.Migrate();
+    }
+}
