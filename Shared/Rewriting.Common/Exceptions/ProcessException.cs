267c24b62600df42600c0bbf43ec0bc51909bfa3
Martivis <vanek-non@rambler.ru>
2023-02-16 20:48:22 +0300

    Add ProcessException

---
+++ Shared/Rewriting.Common/Exceptions/ProcessException.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rewriting.Common.Exceptions
+{
+    public class ProcessException : Exception
+    {
+        /// <summary>
+        ///Error code
+        /// </summary>
+        public string Code { get; }
+
+        /// <summary>
+        /// Error name
+        /// </summary>
+        public string Name { get; }
+
+        public ProcessException()
+        {
+        }
+
+        public ProcessException(string message) : base(message)
+        {
+        }
+
+        public ProcessException(Exception inner) : base(inner.Message, inner)
+        {
+        }
+
+        public ProcessException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        public ProcessException(string code, string message) : base(message)
+        {
+            Code = code;
+        }
+
+        public ProcessException(string code, string message, Exception inner) : base(message, inner)
+        {
+            Code = code;
+        }
+    }
+}
