adf6c99ce43dc72cae56f4a3aa666efecfadbde4
Martivis <vanek-non@rambler.ru>
2023-02-19 15:03:06 +0300

    Add tests

--- Services/Tests/Rewriting.Services.Orders.UnitTests/Helpers/TestDataProvider.cs
+++ Services/Tests/Rewriting.Services.Orders.UnitTests/Helpers/TestDataProvider.cs
@@ -11,17 +11,18 @@ internal static class TestDataProvider
 {
     public static Guid User1Guid => Guid.Parse("eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee");
     public static Guid User2Guid => Guid.Parse("ffffffff-ffff-ffff-ffff-ffffffffffff");
-    public static IEnumerable<Order> Orders =>
+
+    public static ICollection<Order> Orders() =>
         new List<Order>()
         {
             new Order
             {
                 Uid = Guid.Parse("11111111-1111-1111-1111-111111111111"),
-                ClientUid = User1Guid,
+                ClientUid = User2Guid,
                 Title = "TITLE",
                 Comment = "COMMENT",
                 Text = "TEXT",
-                Status = OrderStatus.New,
+                Status = OrderStatus.InProgress,
                 DateTime = DateTime.Parse("01.01.2020")
             },
             new Order
@@ -31,7 +32,7 @@ internal static class TestDataProvider
                 Title = "TITLE",
                 Comment = "COMMENT",
                 Text = "TEXT",
-                Status = OrderStatus.New,
+                Status = OrderStatus.Done,
                 DateTime = DateTime.Parse("01.01.2020")
             },
             new Order
@@ -57,82 +58,57 @@ internal static class TestDataProvider
             new Order
             {
                 Uid = Guid.Parse("55555555-5555-5555-5555-555555555555"),
+                ClientUid = User2Guid,
+                Title = "TITLE",
+                Comment = "COMMENT",
+                Text = "TEXT",
+                Status = OrderStatus.New,
+                DateTime = DateTime.Parse("01.01.2020")
+            }
+        };
+
+    public static ICollection<OrderModel> OrderModels =>
+        new List<OrderModel>
+        {
+            new OrderModel
+            {
+                Uid = Guid.Parse("11111111-1111-1111-1111-111111111111"),
                 ClientUid = User1Guid,
                 Title = "TITLE",
-                Comment = "COMMENT",
-                Text = "TEXT",
                 Status = OrderStatus.New,
                 DateTime = DateTime.Parse("01.01.2020")
             },
-            new Order
+            new OrderModel
             {
-                Uid = Guid.Parse("66666666-6666-6666-6666-666666666666"),
+                Uid = Guid.Parse("22222222-2222-2222-2222-222222222222"),
                 ClientUid = User1Guid,
                 Title = "TITLE",
-                Comment = "COMMENT",
-                Text = "TEXT",
                 Status = OrderStatus.New,
                 DateTime = DateTime.Parse("01.01.2020")
             },
-            new Order
+            new OrderModel
             {
-                Uid = Guid.Parse("77777777-7777-7777-7777-777777777777"),
-                ClientUid = User2Guid,
+                Uid = Guid.Parse("33333333-3333-3333-3333-333333333333"),
+                ClientUid = User1Guid,
                 Title = "TITLE",
-                Comment = "COMMENT",
-                Text = "TEXT",
                 Status = OrderStatus.New,
                 DateTime = DateTime.Parse("01.01.2020")
             },
-            new Order
+            new OrderModel
             {
-                Uid = Guid.Parse("88888888-8888-8888-8888-888888888888"),
-                ClientUid = User2Guid,
+                Uid = Guid.Parse("44444444-4444-4444-4444-444444444444"),
+                ClientUid = User1Guid,
                 Title = "TITLE",
-                Comment = "COMMENT",
-                Text = "TEXT",
                 Status = OrderStatus.New,
                 DateTime = DateTime.Parse("01.01.2020")
             },
-            new Order
+            new OrderModel
             {
-                Uid = Guid.Parse("99999999-9999-9999-9999-999999999999"),
-                ClientUid = User2Guid,
+                Uid = Guid.Parse("55555555-5555-5555-5555-555555555555"),
+                ClientUid = User1Guid,
                 Title = "TITLE",
-                Comment = "COMMENT",
-                Text = "TEXT",
                 Status = OrderStatus.New,
                 DateTime = DateTime.Parse("01.01.2020")
-            },
-            new Order
-            {
-                Uid = Guid.Parse("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"),
-                ClientUid = User2Guid,
-                Title = "TITLE",
-                Comment = "COMMENT",
-                Text = "TEXT",
-                Status = OrderStatus.New,
-                DateTime = DateTime.Parse("01.01.2020")
-            },
-            new Order
-            {
-                Uid = Guid.Parse("bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb"),
-                ClientUid = User2Guid,
-                Title = "TITLE",
-                Comment = "COMMENT",
-                Text = "TEXT",
-                Status = OrderStatus.New,
-                DateTime = DateTime.Parse("01.01.2020")
-            },
-            new Order
-            {
-                Uid = Guid.Parse("cccccccc-cccc-cccc-cccc-cccccccccccc"),
-                ClientUid = User2Guid,
-                Title = "TITLE",
-                Comment = "COMMENT",
-                Text = "TEXT",
-                Status = OrderStatus.New,
-                DateTime = DateTime.Parse("01.01.2020")
-            },
+            }
         };
 }
