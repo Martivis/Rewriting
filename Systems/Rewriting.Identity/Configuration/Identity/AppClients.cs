84baef6812f52d651bf4832d5c1645fd84e6960a
Martivis <vanek-non@rambler.ru>
2023-02-26 22:26:26 +0300

    Increase tokens lifetime

--- Systems/Rewriting.Identity/Configuration/Identity/AppClients.cs
+++ Systems/Rewriting.Identity/Configuration/Identity/AppClients.cs
@@ -30,13 +30,13 @@ public static class AppClients
                 ClientId = "frontend",
                 ClientSecrets = { new Secret("secret".Sha256()), },
                 AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                AccessTokenLifetime = 60,
+                AccessTokenLifetime = 1800, // 30 min
                 AccessTokenType = AccessTokenType.Jwt,
                 AllowOfflineAccess = true,
                 RefreshTokenExpiration = TokenExpiration.Sliding,
                 RefreshTokenUsage = TokenUsage.OneTimeOnly,
-                SlidingRefreshTokenLifetime = 60,
-                AbsoluteRefreshTokenLifetime = 360,
+                SlidingRefreshTokenLifetime = 1296000, // 15 days
+                AbsoluteRefreshTokenLifetime = 1728000, // 20 days
                 AllowedScopes =
                 {
                     AppScopes.OrdersRead,
