2c4657de5a858160fe9070ac4c38182c2a84e2ba
Martivis <vanek-non@rambler.ru>
2023-03-08 15:38:20 +0300

    Add OffersServiceBootstraper

---
+++ Services/Rewriting.Services.Offers/OfferServiceBootstraper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rewriting.Services.Offers
+{
+    public static class OfferServiceBootstraper
+    {
+        public static IServiceCollection AddOfferService(this IServiceCollection services)
+        {
+            services.AddSingleton<IOfferService, OfferService>();
+            return services;
+        }
+    }
+}
