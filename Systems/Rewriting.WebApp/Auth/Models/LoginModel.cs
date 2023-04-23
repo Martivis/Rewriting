f3c8a385fab26f2bb79abfbda8453e979d124e63
Martivis <vanek-non@rambler.ru>
2023-04-23 21:33:36 +0300

    Add login form validation

--- Systems/Rewriting.WebApp/Auth/Models/LoginModel.cs
+++ Systems/Rewriting.WebApp/Auth/Models/LoginModel.cs
@@ -1,9 +1,14 @@
-﻿namespace Rewriting.WebApp;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rewriting.WebApp;
 
 public class LoginModel
 {
+    [Required]
+    [EmailAddress(ErrorMessage = "Invalid email")]
     public string Email { get; set; }
 
+    [Required]
     public string Password { get; set; }
 
     public bool RememberMe { get; set; }
