eb9310e03437f0ac9bcdbd11d1290e1362d5f84f
Martivis <vanek-non@rambler.ru>
2023-04-25 21:31:44 +0300

    WebApp: Add AddOrder page

--- Systems/Rewriting.WebApp/Services/Offers/Models/AddOfferModel.cs
+++ Systems/Rewriting.WebApp/Services/Offers/Models/AddOfferModel.cs
@@ -3,7 +3,6 @@
 public class AddOfferModel
 {
     public Guid OrderUid { get; set; }
-    public Guid ContractorUid { get; set; }
     public decimal Price { get; set; }
     public string Comment { get; set; }
 }
--- Systems/Rewriting.WebApp/Services/Orders/Models/AddOrderModel.cs
+++ Systems/Rewriting.WebApp/Services/Orders/Models/AddOrderModel.cs
@@ -5,5 +5,4 @@ public class AddOrderModel
     public string Title { get; set; }
     public string Text { get; set; }
     public string Comment { get; set; }
-    public Guid ClientUid { get; set; }
 }
