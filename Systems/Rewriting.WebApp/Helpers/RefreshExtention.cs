d48985d3ef6cdf4e6d1ae8c74cc0d0e168e9bc0f
Martivis <vanek-non@rambler.ru>
2023-04-25 18:59:53 +0300

    WebApp: Fix Accept Offer button

---
+++ Systems/Rewriting.WebApp/Helpers/RefreshExtention.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Rewriting.WebApp;
+
+public static class RefreshExtention
+{
+    public static void RefreshPage(this NavigationManager navigationManager)
+    {
+        navigationManager.NavigateTo(navigationManager.Uri, forceLoad: true);
+    }
+}
