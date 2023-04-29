463b78a74e05635211458a3569ee6a7901731b9d
Martivis <vanek-non@rambler.ru>
2023-04-29 21:37:06 +0300

    WebApp: Separate OrderDetails and ContractDetails

--- Systems/Rewriting.WebApp/Services/Orders/Models/OrderDetailsModel.cs
+++ Systems/Rewriting.WebApp/Services/Orders/Models/OrderDetailsModel.cs
@@ -7,10 +7,7 @@ public class OrderDetailsModel
     public string Text { get; set; }
     public string Comment { get; set; }
     public DateTime PublishDate { get; set; }
-    public DateTime? ContractPublishDate { get; set; }
     public OrderStatus Status { get; set; }
     public Guid ClientUid { get; set; }
     public string ClientName { get; set; }
-    public Guid? ContractorUid { get; set; }
-    public string? ContractorName { get; set; }
 }
