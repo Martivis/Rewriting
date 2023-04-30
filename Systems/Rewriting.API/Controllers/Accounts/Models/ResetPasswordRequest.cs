db141e524c2ca6a3c34f7bc2fe7c0ec8ae515cf8
Martivis <vanek-non@rambler.ru>
2023-04-30 23:40:43 +0300

    API: Add reset password validation

--- Systems/Rewriting.API/Controllers/Accounts/Models/ResetPasswordRequest.cs
+++ Systems/Rewriting.API/Controllers/Accounts/Models/ResetPasswordRequest.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using FluentValidation;
 using Rewriting.Services.UserAccount;
 
 namespace Rewriting.API.Controllers.Accounts;
@@ -10,6 +11,23 @@ public class ResetPasswordRequest
     public string NewPassword { get; set; }
 }
 
+public class ResetPasswordValidator : AbstractValidator<ResetPasswordRequest>
+{
+    public ResetPasswordValidator()
+    {
+        RuleFor(p => p.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email");
+
+        RuleFor(p => p.Token)
+            .NotEmpty().WithMessage("Token is required");
+
+        RuleFor(p => p.NewPassword)
+            .NotEmpty().WithMessage("New password is required")
+            .MinimumLength(6).WithMessage("Password should contain at least 6 symbols")
+            .MaximumLength(50).WithMessage("Password is too long");
+    }
+}
 public class ResetPasswordRequestProfile : Profile
 {
     public ResetPasswordRequestProfile()
