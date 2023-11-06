1e773802fcdc5fde822d763a39d97dcf67f3a324
Martivis <vanek-non@rambler.ru>
2023-11-06 22:22:50 +0300

    TextComparer: Implement TextCanonizer

---
+++ Services/Rewriting.Services.TextComparer/Implementations/TextCanonizer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rewriting.Services.TextComparer;
+
+internal class TextCanonizer : ITextCanonizer
+{
+    public List<string> Canonize(string text)
+    {
+        var minWordLength = 3;
+        var words = text.ToLower().Split();
+        var cleanedWords = words.Select(s => new string(s.Where(char.IsLetter).ToArray()));
+        var bigWords = cleanedWords.Where(c => c.Count() > minWordLength);
+
+        return bigWords.ToList();
+    }
+}
